83afcb0e75f4889166971186978df49fdd06bdb6
marlenescobarl <marlenescobarlima11@gmail.com>
2021-03-01 22:00:12 -0600

    se agregaron las opciones para el menu principal

--- src/Ejercicio_dos.java
+++ src/Ejercicio_dos.java
@@ -1,7 +1,21 @@
+import java.util.*;
 public class Ejercicio_dos
 {
     public static void main(String[] args)
     {
+        Scanner go = new Scanner(System.in);
+        //declaracion de variables
+        int opciones = 4, cadena_tamano = 0;
+        String cadena_original = "", sustraccion = "";
+
+        System.out.println("    Que operacion desea");
+        System.out.println("");
+        System.out.println("1. Mostrar el tamanno de la cadena");
+        System.out.println("2. Mostrar una subcadena");
+        System.out.println("3. Comparacion de palabras");
+        System.out.println("4. Comparacion de tamanno");
+        opciones = go.nextInt();
+
 
     }
 }
