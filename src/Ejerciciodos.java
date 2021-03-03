141451506f31dee5714a419821a71b3b813cc190
marlenescobarl <marlenescobarlima11@gmail.com>
2021-03-02 20:34:03 -0600

    Se hicieron correcciones en todo el codigo

--- src/Ejerciciodos.java
+++ src/Ejerciciodos.java
@@ -11,12 +11,17 @@ public class Ejerciciodos
 
         while (!salir)
         {
-            System.out.println("    Elija una opcion");
+            System.out.println("__________________________________");
+            System.out.println("");
+            System.out.println("|------------------------|");
+            System.out.println("|   Elija una opcion     |");
+            System.out.println("|________________________|");
             System.out.println("");
             System.out.println("1. Mostrar el tamanno de la cadena");
             System.out.println("2. Mostrar una subcadena");
             System.out.println("3. Comparacion de palabras");
             System.out.println("4. Comparacion de tamanno");
+            System.out.println("5. Salir");
             op = opcion.nextInt();
 
             //menu
@@ -28,25 +33,32 @@ public class Ejerciciodos
                     cadena = texto.nextLine();
                     tamano = cadena.length();
                     System.out.println("");
-                    System.out.print("El tamaño de la cadena es: " + tamano);
+                    System.out.println("El tamaño de la cadena es: " + tamano);
                     break;
                 case 2:
-                    System.out.println("Cadena original");
-                    System.out.println(cadena);
-                    System.out.println("Desde donde desea optener la nueva cadena");
+                    String cadena_dos = "";
+                    System.out.println("Ingrese una cadena de texto " );
+                    Scanner cad_s = new Scanner(System.in);
+                    cadena_dos = cad_s.nextLine();
+                    System.out.print("Desde donde desea optener la nueva cadena: ");
                     Scanner des = new Scanner(System.in);
                     desde = des.nextInt();
-                    System.out.println("Hasta donde desea optener la nueva cadena");
+                    System.out.print("Hasta donde desea optener la nueva cadena: ");
                     Scanner has = new Scanner(System.in);
                     hasta = has.nextInt();
+                    System.out.println("");
                     if(hasta <= tamano)
                     {
-                        sub = cadena.substring(desde,hasta);
+                        sub = cadena_dos.substring(desde,hasta);
                         System.out.print("La nueva subcadena es: " + sub);
                         System.out.println("");
 
-                    }else {
-                        System.out.println("El tamanno no exede el tamanno de la cadena original");
+                    }else if (hasta < desde)
+                    {
+                        System.out.println("ERROR de ingreso de datos");
+
+                    } else {
+                        System.out.println("El dato ingresado exede el tamanno de la cadena");
                     }
 
                     break;
@@ -96,23 +108,30 @@ public class Ejerciciodos
                     System.out.println("Ingrese una frase");
                     Scanner fras = new Scanner(System.in);
                     String frase = fras.nextLine();
-                    System.out.println("Desea ignorar espacios");
+                    System.out.println("Desea ignorar espacios SI (1) NO (2)");
                     Scanner espace = new Scanner(System.in);
                     int espacios = espace.nextInt();
+
+                    /*
+
                     if(espacios == 1)
                     {
                         int tamano_frase = frase.length();
                         System.out.println("Frase: " + frase + " tamanno: " + tamano_frase);
-                    }else if(espacios == 2)
+                    }else {
+                        System.out.println("Debe ingresar 1 para SI y 2 para NO");
+                    }
+                    if(espacios == 2)
                     {
-                        /* para ignorar los espacios se tiene que trabajar con la misma variable
-                        haciendo uso de las expresiones regulares
-                         */
+                        //para ignorar los espacios se tiene que trabajar con la misma variable
+                        //haciendo uso de las expresiones regulares
+
                         frase = frase.replaceAll("\\s","");
                         System.out.println("Frase: " + frase + " tamanno: " + frase.length());
-
+                    }else {
+                        System.out.println("Debe ingresar 1 para SI y 2 para NO");
                     }
-
+                    */
                     break;
                 case 5:
                     salir = true;
